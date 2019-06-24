67368ada8918b2f0d8fefef3bcdfaa3bb9e04e26
linh.huathi <linhhuacntt@gmail.com>
2019-06-24 16:03:44 -0700

    [PHAN-06] 018 cach viet tong quat tuong tac trong react js

--- 013_component/src/Component/Content/Contents.js
+++ 013_component/src/Component/Content/Contents.js
@@ -10,7 +10,11 @@ class Contents extends Component {
 
     thongbao = () => {alert('Cach xu ly thao tac trong react js ');}
 
-    thongbao2 = () => {alert('thong bao so 2')}
+    thongbao2 = () => {alert('thong bao so 2');}
+
+
+    thongbao3 = (x) => {alert(x);}
+
 
     render() {
         return (
@@ -31,7 +35,8 @@ class Contents extends Component {
                         <div className="row">
                             <div className="btn btn-info" onClick={this.thongbao}>Edit</div>
                             <div className="btn btn-warning" onClick={this.thongbao2}>Remove</div>
-                            
+                            <div className="btn btn-warning" onClick={() => this.thongbao3("ha ha")}>Tb3</div>
+                            <div className="btn btn-warning" onClick={this.thongbao3.bind(this,"ho ho ho")}>Tb4</div>
                           </div>
                           <hr />
                         </div>
