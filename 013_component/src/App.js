8c3ce39129a4b644463cb06291606d47b5b9a574
linh.huathi <linhhuacntt@gmail.com>
2019-06-24 08:57:52 -0700

    [PHAN-05] 015 su dung phim tat cho viec tao component

--- 013_component/src/App.js
+++ 013_component/src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Topmenu from './Component/Topmenu/Topmenu.js';
+import Header from './Component/Header/Header.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="013">
+      <Topmenu/>
+      <Header/>
     </div>
   );
 }
