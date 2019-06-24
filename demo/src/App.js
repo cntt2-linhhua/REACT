3306cd87df979fbd3563999920d3a0e0296a229e
linh.huathi <linhhuacntt@gmail.com>
2019-06-23 17:12:00 -0700

    [PHAN-03] 010 Phuwong phap so 2 thao tac voi props trong component

--- demo/src/App.js
+++ demo/src/App.js
@@ -19,6 +19,23 @@ function NumberOne(props) {
    </div>
   )
 }
+
+class NumberTwo extends Component {
+ render() {
+   return (
+      <div className="col-4">
+        <div className="card">
+          <img className="card-img-top" scr={this.props.linkanh} />
+          <div className="card-body">
+            <h4 className="card-title"> {this.props.tieude} </h4>
+            <p className="card-text">Help people interested in this repository understand your project by adding a README.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -41,6 +58,7 @@ class App extends Component {
           <NumberOne tieude="San pham 1" linkanh="https://kenh14cdn.com/2019/6/19/hoatran-9781-1560913999285308451313.jpg" />
           <NumberOne tieude="San pham 2" linkanh="https://i.pinimg.com/originals/3c/f6/88/3cf6884def2bb5c878622614a13e3c6d.jpg" />
           <NumberOne tieude="Aquamon" linkanh="https://i.pinimg.com/originals/60/e3/1e/60e31e32c3faf797fdd1953f485731bf.jpg" />
+          <NumberTwo tieude="THtruemilk" linkanh="https://ae01.alicdn.com/kf/HTB1CdJzIXXXXXbuXFXXq6xXFXXXB/02-my-neighbor-totoro-cartoon-movie-24-x30-Poster.jpg" />
         </div>
       </div>
       </div>
